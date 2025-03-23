b3090cdb02222038fac4e566cbfd447f49b6b956
Htet Min Lu <htet1234lu@gmail.com>
2025-03-23 21:02:12 +0630

    final

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+
+
     ],
+
+    server: {
+        https: true,
+        host: 'localhost',
+      },
 });
